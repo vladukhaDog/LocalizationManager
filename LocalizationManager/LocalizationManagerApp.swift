bd069b096ba7dc3c86e6bc4d2f938f9c5c64ae80
Vladislav Permyakov <vladic.p@gmail.com>
2022-04-08 15:26:12 +0300

    Initial Commit

---
+++ LocalizationManager/LocalizationManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationManagerApp.swift
+//  LocalizationManager
+//
+//  Created by Permyakov Vladislav on 08.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
